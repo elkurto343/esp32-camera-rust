502a746672ffdbdf6114d87008f9af87707f9c74
kurt <50203404+el-kurto@users.noreply.github.com>
2023-05-08 21:54:10 +1000

    add cfg toml

--- build.rs
+++ build.rs
@@ -1,6 +1,18 @@
-use std::error::Error;
+use std::path;
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[toml_cfg::toml_config]
+pub struct Config {
+    #[default("")]
+    wifi_ssd: &'static str,
+    #[default("")]
+    wifi_psk: &'static str,
+}
+
+fn main() -> anyhow::Result<()> {
+    if !path::Path::new("cfg.toml").exists() {
+        anyhow::bail!("Wi-Fi credentials missing. See cfg.toml.");
+    }
+
     embuild::build::CfgArgs::output_propagated("ESP_IDF")?;
     embuild::build::LinkArgs::output_propagated("ESP_IDF")?;
 
