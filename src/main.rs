ace8f5f4457bb2784dbf2de0b484abe6cd8bf40c
kurt <50203404+el-kurto@users.noreply.github.com>
2023-05-12 14:15:27 +1000

    wifi connection working

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use esp_idf_hal::{gpio::PinDriver, peripherals::Peripherals};
 use esp_idf_svc;
-use esp_idf_svc::eventloop::{EspBackgroundEventLoop, EspSystemEventLoop};
+use esp_idf_svc::eventloop::EspSystemEventLoop;
 use esp_idf_sys::{self as _, esp_camera_fb_get, esp_camera_fb_return, esp_camera_init};
 
 use std::thread;
@@ -39,25 +39,25 @@ fn main() -> anyhow::Result<()> {
         led.set_high()?;
 
         // Capture an image
-        // let fb = unsafe { esp_camera_fb_get() };
-        // if fb.is_null() {
-        //     eprintln!("Failed to capture an image");
-        //     thread::sleep(Duration::from_secs(5));
-        //     continue;
-        // }
-        //
-        // // Base64 encode the image data
-        // let img_data = unsafe { std::slice::from_raw_parts((*fb).buf, (*fb).len as usize) };
-        // let base64_img = base64::engine::general_purpose::STANDARD.encode(img_data);
-        //
-        // // Print the base64 encoded image to console
-        // println!("----------------------------------------------");
-        // println!("Image size: {} KB", img_data.len() / 1024);
-        // println!("----------------------------------------------");
-        // println!("{}", base64_img);
-        //
-        // // Return the frame buffer to the camera driver
-        // unsafe { esp_camera_fb_return(fb) };
+        let fb = unsafe { esp_camera_fb_get() };
+        if fb.is_null() {
+            eprintln!("Failed to capture an image");
+            thread::sleep(Duration::from_secs(5));
+            continue;
+        }
+
+        // Base64 encode the image data
+        let img_data = unsafe { std::slice::from_raw_parts((*fb).buf, (*fb).len as usize) };
+        let base64_img = base64::engine::general_purpose::STANDARD.encode(img_data);
+
+        // Print the base64 encoded image to console
+        println!("----------------------------------------------");
+        println!("Image size: {} KB", img_data.len() / 1024);
+        println!("----------------------------------------------");
+        println!("{}", base64_img);
+
+        // Return the frame buffer to the camera driver
+        unsafe { esp_camera_fb_return(fb) };
 
         led.set_low()?;
 
--- src/wifi.rs
+++ src/wifi.rs
@@ -1,8 +1,12 @@
-use esp_idf_hal::{gpio::PinDriver, modem::Modem, peripheral::Peripheral};
+use esp_idf_hal::{modem::Modem, peripheral::Peripheral};
+use esp_idf_svc::netif::EspNetifWait;
 use esp_idf_svc::{
-    eventloop::{EspBackgroundEventLoop, EspSystemEventLoop},
-    wifi::EspWifi,
+    eventloop::EspSystemEventLoop,
+    wifi::{EspWifi, WifiWait},
 };
+use std::time::Duration;
+// use std::net::Ipv4Addr;
+use embedded_svc::wifi::{AccessPointConfiguration, ClientConfiguration, Configuration};
 
 #[toml_cfg::toml_config]
 pub struct Config {
@@ -16,9 +20,6 @@ pub fn wifi(
     modem: impl Peripheral<P = Modem> + 'static,
     sysloop: EspSystemEventLoop,
 ) -> anyhow::Result<Box<EspWifi<'static>>> {
-    use esp_idf_svc::handle::RawHandle;
-    use std::net::Ipv4Addr;
-
     let config = CONFIG;
 
     let mut wifi = Box::new(EspWifi::new(modem, sysloop.clone(), None)?);
@@ -26,65 +27,52 @@ pub fn wifi(
 
     let ap_infos = wifi.scan()?;
 
-    println!("wifi: ap list");
-    for info in ap_infos.into_iter() {
-        println!("- {}", info.ssid);
-    }
-
+    let mut channel = None;
     let target = ap_infos.into_iter().find(|ap| ap.ssid == config.ssid);
     if let Some(target) = target {
         println!("wifi: ssid matched");
-        Some(target.channel)
-    } else {
-        println!("wifi: ssid not matched");
-        None
+        channel = Some(target.channel)
     }
 
     wifi.set_configuration(&Configuration::Mixed(
-            ClientConfiguration {
-                ssid: config.ssid.into(),
-                password: config.pass.into(),
-                channel,
-                ..Default::default(),
-            },
-            
-            AccessPointConfiguration {
-                ssid: "aptest".into(),
-                channel: channel.unwrap_or(1),
-                ..Default::default(),
-            }
-            ))?;
+        ClientConfiguration {
+            ssid: config.ssid.into(),
+            password: config.pass.into(),
+            channel,
+            ..Default::default()
+        },
+        AccessPointConfiguration {
+            ssid: "aptest".into(),
+            channel: channel.unwrap_or(1),
+            ..Default::default()
+        },
+    ))?;
 
     wifi.start()?;
 
     if !WifiWait::new(&sysloop)?
         .wait_with_timeout(Duration::from_secs(20), || wifi.is_started().unwrap())
     {
-        bail!("wifi: device failed to start");
-
+        println!("wifi: device failed to start");
     }
 
     println!("wifi: connecting to {}", config.ssid);
 
     wifi.connect()?;
 
-    if !EspNetifWait::new::<EspNetif>(wifi.sta_netif(), &sysloop)?.wait_with_timeout(
-        Duration::from_secs(20),
-        || {
-            wifi.is_connected().unwrap()
-                && wifi.sta_netif().get_ip_info().unwrap().ip != Ipv4Addr::new(0, 0, 0, 0)
-        },
-    ) {
-        bail!("wifi: did not connect or did not receive a DHCP lease");
-    }
+    // if !EspNetifWait::new::<EspNetif>(wifi.sta_netif(), &sysloop)?.wait_with_timeout(
+    //     Duration::from_secs(20),
+    //     || {
+    //         wifi.is_connected().unwrap()
+    //             && wifi.sta_netif().get_ip_info().unwrap().ip != Ipv4Addr::new(0, 0, 0, 0)
+    //     },
+    // ) {
+    //     bail!("wifi: did not connect or did not receive a DHCP lease");
+    // }
 
     let ip_info = wifi.sta_netif().get_ip_info()?;
 
-    info!("Wifi DHCP info: {:?}", ip_info);
-
-    // ping(ip_info.subnet.gateway)?;
+    println!("wifi: dhcp info: {:?}", ip_info);
 
     Ok(wifi)
-
-
 }
