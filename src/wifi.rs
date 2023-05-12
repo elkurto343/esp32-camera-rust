b68df8dbc805d57a0f4d7ee80e5028e777239501
kurt <50203404+el-kurto@users.noreply.github.com>
2023-05-12 12:36:44 +1000

    start wifi connection

--- src/wifi.rs
+++ src/wifi.rs
@@ -15,8 +15,7 @@ pub struct Config {
 pub fn wifi(
     modem: impl Peripheral<P = Modem> + 'static,
     sysloop: EspSystemEventLoop,
-) -> anyhow::Result<()> {
-    /* -> anyhow::Result<Box<EspWifi<'static>>> */
+) -> anyhow::Result<Box<EspWifi<'static>>> {
     use esp_idf_svc::handle::RawHandle;
     use std::net::Ipv4Addr;
 
@@ -32,8 +31,60 @@ pub fn wifi(
         println!("- {}", info.ssid);
     }
 
-    // TODO: connect to SSID
+    let target = ap_infos.into_iter().find(|ap| ap.ssid == config.ssid);
+    if let Some(target) = target {
+        println!("wifi: ssid matched");
+        Some(target.channel)
+    } else {
+        println!("wifi: ssid not matched");
+        None
+    }
+
+    wifi.set_configuration(&Configuration::Mixed(
+            ClientConfiguration {
+                ssid: config.ssid.into(),
+                password: config.pass.into(),
+                channel,
+                ..Default::default(),
+            },
+            
+            AccessPointConfiguration {
+                ssid: "aptest".into(),
+                channel: channel.unwrap_or(1),
+                ..Default::default(),
+            }
+            ))?;
+
+    wifi.start()?;
+
+    if !WifiWait::new(&sysloop)?
+        .wait_with_timeout(Duration::from_secs(20), || wifi.is_started().unwrap())
+    {
+        bail!("wifi: device failed to start");
+
+    }
+
     println!("wifi: connecting to {}", config.ssid);
 
-    Ok(())
+    wifi.connect()?;
+
+    if !EspNetifWait::new::<EspNetif>(wifi.sta_netif(), &sysloop)?.wait_with_timeout(
+        Duration::from_secs(20),
+        || {
+            wifi.is_connected().unwrap()
+                && wifi.sta_netif().get_ip_info().unwrap().ip != Ipv4Addr::new(0, 0, 0, 0)
+        },
+    ) {
+        bail!("wifi: did not connect or did not receive a DHCP lease");
+    }
+
+    let ip_info = wifi.sta_netif().get_ip_info()?;
+
+    info!("Wifi DHCP info: {:?}", ip_info);
+
+    // ping(ip_info.subnet.gateway)?;
+
+    Ok(wifi)
+
+
 }
