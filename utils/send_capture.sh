dbb5d8086320de9545a6753ee999e4efda9697fe
kurt <50203404+el-kurto@users.noreply.github.com>
2023-06-18 17:08:27 +1000

    add bash util for sending capture command

---
+++ utils/send_capture.sh
@@ -0,0 +1,15 @@
+# TODO: replace with rust cli tool
+
+#!/bin/bash
+
+if [ -z "$BOARD_IP" ]; then
+	echo "Error: BOARD_IP not set"
+	exit 1
+fi
+
+# Generate a filename with the timestamp
+timestamp=$(date +"%Y%m%d_%H%M%S")
+filename="${timestamp}.jpg"
+
+# Send command to the device and save the response as a .jpg file
+printf '\x01\x00\x00\x00\x00' | nc "$BOARD_IP" 8080 >"captures/$filename"
